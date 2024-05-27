ee9fdf5c1bfb2722b3571d31153a39be4ae04fbe
Mdv17 <methembedvundla17@gmail.com>
2024-05-27 16:40:48 +0200

    second commit

--- VundlaLegacy/js.js
+++ VundlaLegacy/js.js
@@ -1,8 +1,42 @@
 // Swiper Slider
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".hero", {
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+        },
     navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".hero .swiper-button-next",
+    prevEl: ".hero .swiper-button-prev",
     },
 });
 // End Of Swiper Slider 1
+
+// Swiper Slider 2
+var swiper = new Swiper(".project-slider", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+    el: ".project-slider",
+    clickable: true,
+    },
+    navigation: {
+    nextEl: ".project-slider .swiper-button-next",
+    prevEl: ".project-slider .swiper-button-prev",
+    },
+    breakpoints: {
+        640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        },
+        768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+        },
+        1024: {
+        slidesPerView: 2,
+        spaceBetween: 50,
+        },
+    }
+});
+
+// End Of Swiper Slider 2
