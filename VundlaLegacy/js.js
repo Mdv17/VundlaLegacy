ebe34d0a5aa5b8a0095dac42b59f5bf63dc4b0c7
Mdv17 <methembedvundla583@gmail.com>
2024-05-27 05:22:05 +0200

    first commit

---
+++ VundlaLegacy/js.js
@@ -0,0 +1,8 @@
+// Swiper Slider
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    },
+});
+// End Of Swiper Slider 1
